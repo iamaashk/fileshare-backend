3f7c0618289add13a5db7dc316d77033e75d7a02
iamaashk <adnanahmadshaikh2003@gmail.com>
2024-02-21 22:46:11 +0530

    v1.0.3

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -8,7 +8,7 @@ export const uploadImage = async (request, response)=>{
     }
     try {
        const  file = await File.create(fileObj);
-       response.status(200).json({path:`fileshare-iamaashk.netlify.app/file/${file._id}`});
+       response.status(200).json({path:`fileshare-backend-iamaashk.netlify.app/file/${file._id}`});
         
     } catch (error) {
         console.error(error.message);
