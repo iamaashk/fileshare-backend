96911b3113785650dba926f707436e882b8cbd8f
iamaashk <adnanahmadshaikh2003@gmail.com>
2024-02-21 22:24:33 +0530

    v1.0.2

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL =`mongodb+srv://iamaashk:AdnanAhmad11@fileshare.rcrurja.mongodb.net/?retryWrites=true&w=majority`;
+const MONGODB_URL =`mongodb://iamaashk:@AdnanAhmad11@ac-nnmx4ts-shard-00-00.rcrurja.mongodb.net:27017,ac-nnmx4ts-shard-00-01.rcrurja.mongodb.net:27017,ac-nnmx4ts-shard-00-02.rcrurja.mongodb.net:27017/?ssl=true&replicaSet=atlas-1287v5-shard-0&authSource=admin&retryWrites=true&w=majority&appName=fileshare`;
 
 const DBConnection = async ()=>{
     try {
